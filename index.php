f39ef0ffee26f4d6fbfdf70e696c08a755531987
Tilmann Ruppert <tilmannruppert@MacBook-Pro-von-Tilmann-2.local>
2025-03-17 12:48:10 +0100

    1.0.2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ use Less_Parser;
 require_once kirby()->root('index') . "/vendor/wikimedia/less.php/lib/Less/Autoloader.php";
 \Less_Autoloader::register();
 
-\Kirby::plugin('tilmannruppert/uikit-blocks', [
+App::plugin('tilmannruppert/uikit-blocks', [
     'routes' => [
         [
             'pattern' => '/assets/js/uikit.min.js',
